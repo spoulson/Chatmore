319c4989833bb4f420179ee10d6cc7c2ba85cd9e
Shawn Poulson <spoulson@explodingcoder.com>
2012-02-16 13:05:04 -0800

    10: Implemented start.html as landing page for prompting connection details and launching client.

--- config.js
+++ config.js
@@ -1,71 +1,7 @@
-$(function () {
-    // Default configuration.
-    var defaults = {
-        server: 'irc.dsm.org',
-        channel: undefined,
-        mustMatchServer: false
-    };
-    
-    // Parse querystring.
-    var opts = {};
-    if (querystring !== undefined) {
-        var form = {};
-        var pairs = querystring.split('&');
-        
-        for (var i in pairs) {
-            var pair = pairs[i].split('=');
-            var key = decodeURIComponent(pair[0]);
-            var value = decodeURIComponent(pair[1]);
-            
-            if (typeof(form[key]) === 'string') {
-                form[key] = [ form[key], value ];
-            }
-            else if (typeof(form[key]) === 'object') {
-                form[key].push(value);
-            }
-            else {
-                form[key] = value;
-            }
-        }
-        
-        if (form.server !== undefined) opts.server = form.server;
-        if (form.port !== undefined) opts.port = form.port;
-        if (form.nick !== undefined) opts.nick = form.nick;
-    }
-    
-    // Parse anchor string.
-    var m = window.location.href.match(/(#.+)/);
-    if (m !== null) {
-        opts.channel = m[1].split(',');
-    }
-    
-    // Determine if any options were set.
-    // If not, assume defaults.
-    var objectSize = function (obj) {
-        var count = 0;
-        for (var prop in obj) {
-            if (obj.hasOwnProperty(prop)) count++;
-        }
-        return count;
-    };
-    
-    var clone;
-    clone = function(obj) {
-        var newObj = (obj instanceof Array) ? [] : {};
-        for (i in obj) {
-            if (obj[i] && typeof(obj[i]) === "object")
-                newObj[i] = clone(obj[i]);
-            else
-                newObj[i] = obj[i];
-        }
-        return newObj;
-    };
-    
-    if (objectSize(opts) == 0) opts = clone(defaults);
-    else opts.mustMatchServer = true;
-    
-    // TODO: If no server provided, popup dialog requesting connection details.
-    
-    // Startup the IRC client.
-    $('.chatmore').chatmore(opts);
-});
+// Default configuration.
+var chatmoreDefaults = {
+    server: 'irc.dsm.org',
+    channel: undefined,
+    mustMatchServer: false
+};
+
--- index.php
+++ index.php
@@ -21,10 +21,6 @@ if (array_key_exists('x', $_GET)) {
     <base href="<?=$scriptPath?>/" />
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="stylesheet" type="text/css" href="themes/atwood/atwood.css" />
-    <script type="text/javascript">
-        // querystring needed for config.js.
-        var querystring = '<?=$_SERVER['QUERY_STRING']?>';
-    </script>
     <script type="text/javascript" src="jquery-1.7.1.min.js"></script>
     <script type="text/javascript" src="jquery-ui-1.8.16.min.js"></script>
     <script type="text/javascript" src="jquery.tmpl.min.js"></script>
@@ -34,7 +30,65 @@ if (array_key_exists('x', $_GET)) {
     <script type="text/javascript" src="config.js"></script>
     <script type="text/javascript">
         $(function () {
+            // Parse querystring.
+            var opts = {};
+            var form = {};
+            var pairs = '<?=$_SERVER['QUERY_STRING']?>'.split('&');
+            
+            for (var i in pairs) {
+                var pair = pairs[i].split('=');
+                var key = decodeURIComponent(pair[0]);
+                var value = decodeURIComponent(pair[1]);
+                
+                if (typeof(form[key]) === 'string') {
+                    form[key] = [ form[key], value ];
+                }
+                else if (typeof(form[key]) === 'object') {
+                    form[key].push(value);
+                }
+                else {
+                    form[key] = value;
+                }
+                
+                if (form.server !== undefined) opts.server = form.server;
+                if (form.port !== undefined) opts.port = form.port;
+                if (form.nick !== undefined) opts.nick = form.nick;
+            }
+            
+            // Parse anchor string.
+            var m = window.location.href.match(/(#.+)/);
+            if (m !== null) {
+                opts.channel = m[1].split(',');
+            }
+            
+            // Determine if any options were set.
+            // If not, assume defaults.
+            var objectSize = function (obj) {
+                var count = 0;
+                for (var prop in obj) {
+                    if (obj.hasOwnProperty(prop)) count++;
+                }
+                return count;
+            };
+            
+            var clone;
+            clone = function(obj) {
+                var newObj = (obj instanceof Array) ? [] : {};
+                for (i in obj) {
+                    if (obj[i] && typeof(obj[i]) === "object")
+                        newObj[i] = clone(obj[i]);
+                    else
+                        newObj[i] = obj[i];
+                }
+                return newObj;
+            };
+            
+            if (objectSize(opts) == 0) opts = clone(chatmoreDefaults);
+            else opts.mustMatchServer = true;
+            
+            // Startup the IRC client.
             var ircElement = $('.chatmore');
+            ircElement.chatmore(opts);
 
             // Stretch client element to width/height of browser window space.
             var stretchClient = function () {
